4aeb1e1d8ff2b732f74b231fb8ee94e86393c333
nshukla2019 <nupi.shukla@gmail.com>
2021-05-18 12:03:35 -0400

    variables in python

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,27 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+x = 1             # int
+y = 2.5           # float
+name = 'John'     # str
+is_cool = True    # bool (True or False)
+
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, 'John', True)
+
+
+# Basic math
+a = x + y
+
+print(x, y, name, is_cool, a)
+
+# Casting
+x = str(x)
+y = int(y)
+
+# type function which tells you the type
+print(type(x))
+
+print(type(y), y)
