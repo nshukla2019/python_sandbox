b7e8727d4de112c60c18fd9f18a3417eec2168bb
nshukla2019 <nupi.shukla@gmail.com>
2021-05-18 14:54:44 -0400

    files (opening, reading, writing, appending, closing)

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,28 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# open a file (open() actually creates it)
+myFile = open('myfile.txt', 'w')
+
+# get some info
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I do not know')
+myFile.write(' and I do not care')
+
+myFile.close()
+
+# append to file (mode is 'a')
+
+myFile = open('myfile.txt', 'a')
+myFile.write(' and I do not think')
+
+myFile.close()
+
+# reading from file
+
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
